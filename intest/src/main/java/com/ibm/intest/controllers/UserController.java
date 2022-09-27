87b85d399d3e64999d8aad09ae1965a5c61a55e8
MimmoL <kynzomail@gmail.com>
2022-09-27 10:35:31 +0200

    Rimosse chiamate rest hardcoded

--- intest/src/main/java/com/ibm/intest/controllers/UserController.java
+++ intest/src/main/java/com/ibm/intest/controllers/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -28,15 +27,11 @@ public class UserController {
     UserGetDtoMapper userGetDtoMapper;
 
 
-    //Risposta hardcoded
-    @GetMapping("/test/get-all-dto")
-    public ResponseEntity<List<UserGetDto>> getAllUsersDtoTest(){
-        User user1 = new User(10007L, "Giuliano", "Verdini", 48, "LPONMP");
-        User user2 = new User(10008L, "Paolo", "Giusti", 33, "JIHNXZ");
-        List<User> users = new ArrayList<>();
-        users.add(user1);
-        users.add(user2);
-        return new ResponseEntity<>(userGetDtoMapper.toUsergetDtoList(users),HttpStatus.OK);
+
+
+    @GetMapping("/get-all")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userService.getAllUsers(),HttpStatus.OK);
     }
 
     @GetMapping("/get-all-dto")
@@ -44,6 +39,7 @@ public class UserController {
         return new ResponseEntity<>(userGetDtoMapper.toUsergetDtoList(userService.getAllUsers()),HttpStatus.OK);
     }
 
+    /*
     //Risposta hardcoded
     @GetMapping("/test/get-all")
     public ResponseEntity<List<User>> getAllUsersTest(){
@@ -55,5 +51,18 @@ public class UserController {
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
+    //Risposta hardcoded
+    @GetMapping("/test/get-all-dto")
+    public ResponseEntity<List<UserGetDto>> getAllUsersDtoTest(){
+        User user1 = new User(10007L, "Giuliano", "Verdini", 48, "LPONMP");
+        User user2 = new User(10008L, "Paolo", "Giusti", 33, "JIHNXZ");
+        List<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+        return new ResponseEntity<>(userGetDtoMapper.toUsergetDtoList(users),HttpStatus.OK);
+    }
+
+
+     */
 
 }
