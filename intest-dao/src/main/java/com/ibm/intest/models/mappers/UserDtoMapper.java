57b11b96c41722fbe4a3949718bf2387f5d9a36e
MimmoL <kynzomail@gmail.com>
2022-09-28 11:02:25 +0200

    Completato il trasferimento del mapper all'interno di intent-api-impl, isolando l'entity dal controller

--- intest-dao/src/main/java/com/ibm/intest/models/mappers/UserDtoMapper.java
+++ intest-dao/src/main/java/com/ibm/intest/models/mappers/UserDtoMapper.java
@@ -5,9 +5,8 @@ import com.ibm.intest.models.entities.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import javax.swing.text.html.Option;
+
 import java.util.List;
-import java.util.Optional;
 
 @Mapper(componentModel = "spring")
 public interface UserDtoMapper {
@@ -18,13 +17,6 @@ public interface UserDtoMapper {
     @Mapping(source = "age", target = "age")
     UserDto toUserDto(User user);
 
-    default Optional<User> wrapOptional(User user){
-        return Optional.of(user);
-    }
-
-    default Optional<UserDto> wrapOptionalDto(UserDto userDto){
-        return Optional.of(userDto);
-    }
 
 
     List<UserDto> toUserDtoList(List<User> users);
